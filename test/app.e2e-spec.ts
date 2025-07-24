d8d612cf41ee3cd3276d096d1933689edad96fb5
ghost <next.nn93@gmail.com>
2025-07-24 01:59:01 -0400

    chore: :construction_worker: octava prueba de flujo

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,6 +20,10 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!')
+      .expect({
+        status: 'success',
+        message: 'Success',
+        data: 'Service is running.',
+      })
   })
 })
